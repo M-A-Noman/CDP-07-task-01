cd4d2318208fbd652f7a7263a4f69a7d7bd07169
M-A-Noman <mdabunomanma@gmail.com>
2024-02-19 16:32:39 +0600

    feddback-button-and-feedback-form-added-with-js

---
+++ script.js
@@ -0,0 +1,8 @@
+function contact(){
+    document.getElementById("feedback").style.display = 'none';
+    document.getElementById("cnt").style.display = 'block';
+}
+function form_show(){
+    document.getElementById("cnt").style.display = 'none';
+    document.getElementById("feedback").style.display = 'block';
+}
